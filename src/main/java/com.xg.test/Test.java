a7030e236783c044a950090135b9f82eb92c9b8b
Style-lo <375277869@qq.com>
2018-04-07 11:04:39 +0800

    Initial commit

---
+++ src/main/java/com.xg.test/Test.java
@@ -0,0 +1,13 @@
+package src.main.java.com.xg.test;
+
+/**
+ * Created by Administrator on 2018-04-07.
+ */
+public class Test {
+    public static void main(String[] args) {
+        int i =0;
+        System.out.println("测试"+i++);
+        System.out.println("测试"+i++);
+        System.out.println("测试"+i++);
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<html>
+<body>
+<h2>Hello World!</h2>
+asdaslakdjls；是是的冯绍峰世纪东方黑客技术的恢复见多识广和款到发货
+地方公开的房间拉开距离打放假老地方赶进度付款了立刻家得房率高考dasda
+adasdhaih 卢克了单身快乐粪就是的数据库斐林试剂我我驾驶的佛教奥是附加
+as肯德基案例集阿斯利康的简历
+</body>
+</html>
